0b57d07027e93cb4890dcf44944232789034631f
Hussein <cloundlint@outlook.com>
2025-07-11 15:12:10 +0200

    update on backup

--- scripts/backup_db.sh
+++ scripts/backup_db.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-# === CONFIGURATION ===
+# Set constants
+BACKUP_DIR="/Volumes/Data/Business/scootdr/db_backups"
+DATE=$(date +%F)
+FILENAME="scooter_backup_$DATE.sql"
 DB_NAME="scooter_db2"
 DB_USER="scooteruser"
 DB_HOST="localhost"
-BACKUP_DIR="/Volumes/Data/Business/scootdr/db_backups"
-DATE=$(date +"%Y-%m-%d_%H-%M-%S")
-FILENAME="scooter_db2_${DATE}.sql"
+DB_PORT="5432"
 
-# === CREATE BACKUP ===
-pg_dump -U "$DB_USER" -h "$DB_HOST" "$DB_NAME" > "$BACKUP_DIR/$FILENAME"
+# Create backup using .pgpass for authentication
+pg_dump -U $DB_USER -h $DB_HOST -p $DB_PORT -f "$BACKUP_DIR/$FILENAME" $DB_NAME
 
-# === DELETE OLD BACKUPS (OLDER THAN 3 DAYS) ===
-find "$BACKUP_DIR" -name "*.sql" -type f -mtime +3 -exec rm {} \;
-
-# === LOGGING ===
-echo "[$(date)] Backup created: $FILENAME" >> "$BACKUP_DIR/backup.log"
+# Remove backups older than 3 days
+find "$BACKUP_DIR" -name "scooter_backup_*.sql" -type f -mtime +3 -exec rm {} \;
